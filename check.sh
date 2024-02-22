70517db2e0b481c06775e30be44d316826ffc378
Miquel Serra Llobera <misello@gmail.com>
2024-02-22 14:03:22 +0100

    Main script
    Added main script

---
+++ check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script shows style suggestions on your go code.
+
+clear
+date +***\ %d-%m-%Y\ %H:%M:%S\ ***
+echo "*** gocyclo -over 15 ***"
+gocyclo -over 15 .
+echo "*** goimports -d ***"
+goimports -d .
+echo "*** misspell ***"
+misspell .
+echo "*** golint ***"
+golint .
+
+#golangci-lint run main.go
